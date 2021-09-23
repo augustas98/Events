5d75a36923b21d0d42c18c982ceb04e088f00094
augustas98 <am851117@gmail.com>
2021-09-23 22:29:01 +0300

    temp

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,34 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import './styles.css';
+import axios from 'axios';
+import { Container, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+
+function App() {
+const [activities, setActivities] = useState<Activity[]>([]);
+
+useEffect(() => {
+  axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+    setActivities(response.data);
+  })
+}, [])
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+      <List>
+        {activities.map(activity => (
+          <List.Item key ={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+      </Container>
+      
+    </Fragment>
+  );
+}
+
+export default App;
